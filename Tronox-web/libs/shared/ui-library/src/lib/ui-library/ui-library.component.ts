ddcfda68c39d66c5e1588198aaf1b8f0b87db660
Lav O Pandey <lav.pandey@in.ey.com>
2025-02-11 15:23:42 +0530

    Deleted extra angular project and added a new ui-library with configs

---
+++ Tronox-web/libs/shared/ui-library/src/lib/ui-library/ui-library.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ui-library',
+  imports: [CommonModule],
+  templateUrl: './ui-library.component.html',
+  styleUrl: './ui-library.component.css',
+})
+export class UiLibraryComponent {}
